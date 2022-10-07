774936c14b660c249a02286d016f390e866e7c2b
etherealsheep <g.perrotta79@gmail.com>
2022-10-07 17:05:41 +0000

    add function to generate opponent choice

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,3 +16,23 @@ gameChoices.forEach(choice => choice.addEventListener('click', (btn_click) => {
     // also generate choice for the opponent
     // also generate the result
 }))
+
+// STEP 2
+//**funtion to generate random selection for the opponent */
+function generateOpponentChoice() {
+    // https://www.w3schools.com/js/js_random.asp
+    let randomNumber = Math.floor(Math.random() * 5) + 1;
+    if (randomNumber === 1) {
+        opponentChoice = 'rock'
+    } else if (randomNumber === 2) {
+        opponentChoice = 'paper'
+    } else if (randomNumber === 3) {
+        opponentChoice = 'scissors'
+    } else if (randomNumber === 4) {
+        opponentChoice = 'lizard'
+    } else {
+        opponentChoice = 'spock'
+    }
+    // without this the opponent choice wont be shown
+    opponentChoiceShow.innerHTML = opponentChoice;
+}
