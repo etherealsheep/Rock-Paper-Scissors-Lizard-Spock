b830aae66a661d18b0399b1d8f3184e245001fb8
etherealsheep <g.perrotta79@gmail.com>
2022-10-24 09:53:04 +0000

    Add comment to js file functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,22 @@
-// STEP 1
-// create a function to allow player choice the same button value
-// e.g.; if spock button is pressed, assign spock_value to player choice
 const gameChoices = document.querySelectorAll('button');
 const p1ChoiceShow = document.getElementById('P1_choice');
 const p2ChoiceShow = document.getElementById('P2_choice');
 const resultShow = document.getElementById('match_result');
-// const resetGame = document.getElementById('reset');
 let p1Choice;
 let p2Choice;
 let result;
 let p1Score = 0;
 let p2Score = 0;
 
-// when the function is called, it also calls functions to assign opponent value choice and generate result
 gameChoices.forEach(choice => choice.addEventListener('click', (btn_click) => {
     p1Choice = btn_click.target.id
     p1ChoiceShow.innerHTML = p1Choice;
-    // also generate choice for the opponent
     generatep2Choice();
-    // also generate the result
     generateResult();
-    // increase the winner score
     increaseWinnerScore()
 }))
 
-// STEP 2
-// funtion to generate random selection for the opponent
+/*** funtion to generate random selection for the opponent */
 function generatep2Choice() {
     // https://www.w3schools.com/js/js_random.asp
     let randomNumber = Math.floor(Math.random() * 5) + 1;
@@ -40,15 +31,13 @@ function generatep2Choice() {
     } else {
         p2Choice = 'spock'
     }
-    // without this, the opponent choice won't be displayed
     p2ChoiceShow.innerHTML = p2Choice;
 }
 
-// STEP 3
-// funtion to determine the result of every choice
+/*** funtion to determine the result of every choice */
 function generateResult() {
     if (p1Choice === p2Choice) {
-        result = 'IT\'S A DRAW!'
+        result = 'DRAW!'
     } else if (p1Choice === 'rock' && p2Choice === 'paper') {
         result = 'YOU LOSE!'
     } else if (p1Choice === 'rock' && p2Choice === 'scissors') {
@@ -90,12 +79,10 @@ function generateResult() {
     } else if (p1Choice === 'spock' && p2Choice === 'lizard') {
         result = 'YOU LOSE!'
     }
-    // without this, the result won't be displayed
     resultShow.innerHTML = result;
 }
 
-// STEP 4
-// create function to determine score value
+/*** function to determine score value */ 
 function increaseWinnerScore() {
     let calculateResult = document.getElementById('match_result').innerHTML;
     if (calculateResult === 'YOU WIN!') {
